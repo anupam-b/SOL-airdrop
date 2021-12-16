6175b72ed4acfff8d72e3392b8bb00c7f0efb476
anupam-b <anupambaranwal@yahoo.com>
2021-12-17 00:33:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-const {
-  Connection,
-  PublicKey,
-  clusterApiUrl,
-  Keypair,
-  LAMPORTS_PER_SOL,
-  Transaction,
-  Account,
-} = require("@solana/web3.js");
+const {Connection,PublicKey, LAMPORTS_PER_SOL} = require("@solana/web3.js");
+const solanaWeb3 = require("@solana/web3.js"),
 
-const newPair = new Keypair();
+//STEP-1 Generating a new wallet keypair
+newPair = new solanaWeb3.Keypair();
 console.log(newPair);
 
+//STEP-2 Storing the public and private key
 const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
 const secretKey = newPair._keypair.secretKey;
 
+//STEP-3 Getting the wallet Balance
 const getWalletBalance = async () => {
   try {
-    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-    const myWallet = await Keypair.fromSecretKey(secretKey);
+    const connection = new Connection(solanaWeb3.clusterApiUrl("devnet"), "confirmed");
     const walletBalance = await connection.getBalance(
-      new PublicKey(myWallet.publicKey)
+      new PublicKey(publicKey)
     );
     console.log(`=> For wallet address ${publicKey}`);
     console.log(`   Wallet balance: ${parseInt(walletBalance)/LAMPORTS_PER_SOL}SOL`);
@@ -28,14 +23,14 @@ const getWalletBalance = async () => {
   }
 };
 
+//STEP-4 Air dropping SOL (in terms of LAMPORTS)
 const airDropSol = async () => {
   try {
-    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-    const walletKeyPair = await Keypair.fromSecretKey(secretKey);
-    console.log(`-- Airdropping 5 SOL --`)
+    const connection = new Connection(solanaWeb3.clusterApiUrl("devnet"), "confirmed");
+    console.log(`-- Airdropping SOL --`)
     const fromAirDropSignature = await connection.requestAirdrop(
-      new PublicKey(walletKeyPair.publicKey),
-      5 * LAMPORTS_PER_SOL
+      new PublicKey(publicKey),
+      2 * LAMPORTS_PER_SOL
     );
     await connection.confirmTransaction(fromAirDropSignature);
   } catch (err) {
@@ -43,6 +38,7 @@ const airDropSol = async () => {
   }
 };
 
+//STEP-5 Driver function
 const driverFunction = async () => {
     await getWalletBalance();
     await airDropSol();
